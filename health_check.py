79bb288a872a924feb51a06385e36527770c3cc8
kr19al0711 <110184961+kr19al0711@users.noreply.github.com>
2022-08-07 09:38:07 +0530

    Update health_check.py

--- health_check.py
+++ health_check.py
@@ -2,8 +2,9 @@
 
 import shutil
 import psutil
+import os
 
-path = "/"
+path = os.cwd()
 disk_usage_stats = shutil.disk_usage(path)
 
 print("Total Disk Utilisation : {}".format(disk_usage_stats.free/disk_usage_stats.total))
